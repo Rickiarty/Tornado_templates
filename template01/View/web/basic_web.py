9268f716accdc17e9f5a72bb075c4b724dd5ced1
Rickiarty <fatalframe0719@gmail.com>
2022-12-13 00:09:03 +0800

    template01 v2.8.0

--- template01/View/web/basic_web.py
+++ template01/View/web/basic_web.py
@@ -1,19 +1,29 @@
-import json
-from urllib.parse import parse_qs
+#import json
+#from urllib.parse import parse_qs
 import View.BaseHandler
 
 class BasicWebHandler(View.BaseHandler.BaseHandler):
     # HTTP method 'GET'
     def get(self):
         self.set_default_headers()
-        http_response_str  = ''
-        http_response_str += "\nrequest URL:\n" + str(self.request.full_url())
-        http_response_str += '\n\nHTTP method/verb:\n' + str(self.request.method)
-        http_response_str += "\n\nHTTP request's header:\n" + str(self.request.headers)
-        http_response_str += '\n\nremote IP:\n' + str(self.request.remote_ip)
-        http_response_str += "\n\nHTTP request's body:\n" + str(self.request.body)
-        http_response_str += "\n\narguments of HTTP request's body:\n" + str(self.request.body_arguments)
-        self.write(http_response_str)
+        #print(self.request.full_url()) # DEBUG 
+        web_url = self.request.full_url()
+        web_url = web_url.replace('http://', '')
+        web_url = web_url.replace('https://', '')
+        hostname_portnumber = web_url.split('/')[0] # 0th element 
+        #print(hostname_portnumber) # DEBUG 
+        web_url = web_url.replace(hostname_portnumber, '')
+        #print(web_url) # DEBUG 
+        file_path = './websrc/' + web_url[1:]
+        #print(file_path) # DEBUG 
+        try:
+            with open(file_path, "rb") as web_source_file: # "rb": read-only and read in binary 
+                http_response_bytes = web_source_file.read() # -> bytes 
+            self.write(http_response_bytes) # write a series of data to network stream in byte - executed only once 
+        except Exception as ex:
+            print(str(ex))
+            self.set_status(404)
+            self.finish("Woops! page not found!")
 
     # HTTP method 'POST'
     def post(self):
@@ -25,7 +35,7 @@ class BasicWebHandler(View.BaseHandler.BaseHandler):
         http_response_str += '\n\nremote IP:\n' + str(self.request.remote_ip)
         http_response_str += "\n\nHTTP request's body:\n" + str(self.request.body)
         http_response_str += "\n\narguments of HTTP request's body:\n" + str(self.request.body_arguments)
-        self.write(json.dumps(http_response_str))
+        self.write(http_response_str) # write a string to network stream - executed only once 
 
     # HTTP method 'OPTIONS'
     def options(super):
--- template01/app.py
+++ template01/app.py
@@ -10,9 +10,8 @@ from View.web import basic_web
 def make_app():
     static_path_dir = './websrc/'
     return tornado.web.Application([
-        (r'^/webapi/.+$', basic_web_api.BasicWebAPIHandler),
-        (r'^/.+\.htm$', basic_web.BasicWebHandler),
-        #(r'.*', basic_web.BasicWebHandler), 
+        (r'^/webapi/.+$', basic_web_api.BasicWebAPIHandler), 
+        (r'.*', basic_web.BasicWebHandler), # To resolve issues 'HTTP 405 - method not allow', it's one of keys to implement this instead of simply using Tornado's mapping mechanism from web URLs to static files' file paths. 
         (r'^/(.*)$', tornado.web.StaticFileHandler, {'path': static_path_dir}),
     ])
 
