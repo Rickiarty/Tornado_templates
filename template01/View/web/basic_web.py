865cd485a997828dafd307ef3e0fbabbadbf8f50
Rickiarty <fatalframe0719@gmail.com>
2023-02-07 04:50:31 +0800

    template01 v4.5.0

--- template01/View/web/basic_web.py
+++ template01/View/web/basic_web.py
@@ -1,38 +1,36 @@
-#import json
-#from urllib.parse import parse_qs
 import View.BaseHandler
+import os
 
 class BasicWebHandler(View.BaseHandler.BaseHandler):
     # HTTP method 'GET'
     def get(self):
         self.set_default_headers()
-        #print(self.request.full_url()) # DEBUG 
         web_url = self.request.full_url()
-        web_url = web_url.replace('http://', '')
-        web_url = web_url.replace('https://', '')
-        hostname_portnumber = web_url.split('/')[0] # 0th element 
-        #print(hostname_portnumber) # DEBUG 
+        web_url = web_url.split('://')[1]
+        web_url = web_url.split('?')[0]
+        web_url = web_url.split('#')[0]
+        url_segs = web_url.split('/')
+        hostname_portnumber = url_segs[0]
+        file_path = ""
         web_url = web_url.replace(hostname_portnumber, '')
-        #print(web_url) # DEBUG 
-        file_path = './websrc/' + web_url[1:]
-        #print(file_path) # DEBUG 
+        if web_url[1:] == 'index.html' or web_url[1:] == 'index.htm' or web_url[1:] == 'index' or web_url[1:] == '' or len(url_segs) == 1:
+            file_path = os.fspath('./websrc/index.html')
+        else:
+            file_path = os.path.join(os.fspath('./websrc/'), web_url[1:])
+        http_response_bytes = None
         try:
             with open(file_path, "rb") as web_source_file: # "rb": read-only and read in binary 
                 http_response_bytes = web_source_file.read() # -> bytes 
-            self.write(http_response_bytes) # write a series of data to network stream in byte - executed only once 
+                #print(file_path, len(http_response_bytes), 'bytes') # DEBUG
+            if http_response_bytes == None:
+                self._httpResponseHandler(404)
+            else:
+                self.write(http_response_bytes) # write a series of data to network stream in byte - executed only once 
+                self.flush()
         except Exception as ex:
             print(str(ex))
-            self.set_status(404)
-            self.finish("Woops! page not found!")
+            self._httpResponseHandler(404)
 
     # HTTP method 'POST'
     def post(self):
-        self.set_default_headers()
-        http_response_str  = ''
-        http_response_str += "\nrequest URL:\n" + str(self.request.full_url())
-        http_response_str += '\n\nHTTP method/verb:\n' + str(self.request.method)
-        http_response_str += "\n\nHTTP request's header:\n" + str(self.request.headers)
-        http_response_str += '\n\nremote IP:\n' + str(self.request.remote_ip)
-        http_response_str += "\n\nHTTP request's body:\n" + str(self.request.body)
-        http_response_str += "\n\narguments of HTTP request's body:\n" + str(self.request.body_arguments)
-        self.write(http_response_str) # write a string to network stream - executed only once 
+        self._httpResponseHandler(204)
